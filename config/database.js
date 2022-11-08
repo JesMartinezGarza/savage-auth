221b4968335c25433d626b0cbfa289fe955d0c63
Jesus Martinez-Garza <jes.martinez.garza@gmail.com>
2022-11-08 17:41:55 -0500

    URL was changed and now functional.

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://axisMundi:mFGpPJbo76y9rtB5@cluster0.xumwt9o.mongodb.net/?retryWrites=true&w=majority', 
+    'url' : 'mongodb+srv://axisMundi:mFGpPJbo76y9rtB5@cluster0.xumwt9o.mongodb.net/Authenticate?retryWrites=true&w=majority', 
     'dbName': 'Authenticate'
 };
